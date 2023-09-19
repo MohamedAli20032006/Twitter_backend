3902e43158d258d100026b9df70d51cdfee105c9
محمد علي محمد <you@example.com>
2023-09-19 21:38:38 +0300

    adding home_view function in tweets views

--- clone/tweetme/tweets/views.py
+++ clone/tweetme/tweets/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home_view(requests, *args, **kwargs):
+    return HttpResponse("<h1>I love you Nada</h1>")
